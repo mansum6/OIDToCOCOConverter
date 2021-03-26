fbe2ab3fa6ca5f96397424b10a11ffac50210ddf
mansum6 <31364532+mansum6@users.noreply.github.com>
2021-03-26 03:07:23 -0600

    Update createSizes.py

--- createSizes.py
+++ createSizes.py
@@ -8,5 +8,5 @@ with open('sizes.csv','wb') as file:
     img = cv2.imread(os.path.join(folder,filename))
     if img is not None:
       h, w, c = img.shape
-      file.write(filename.split(".")[0]+","+str(w)+","+str(h))
+      file.write((filename.split(".")[0]+","+str(w)+","+str(h)).encode('utf-8'))
 file.close    
